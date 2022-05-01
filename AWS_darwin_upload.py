9ce0d13a4f56681c3f1dea94e8f44260b958c681
alanbalugu <55846088+alanbalugu@users.noreply.github.com>
2022-05-01 09:27:35 -0400

    Add files via upload

---
+++ AWS_darwin_upload.py
@@ -0,0 +1,90 @@
+import requests
+import sys
+
+api_key = "DmEt_0-.ijvbnnYicCjTLntK2R3UQwFGcdIJaSzO"
+
+team_slug = "surgical-data-science-collaborative"
+dataset_slug = "sospine"
+storage_name = "donoholab1"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"ApiKey {api_key}"
+}
+
+def upload(type, AWS_file_name):
+
+    print("uploading...", AWS_file_name, end=" ")
+
+    if(len(AWS_file_name) < 2): exit()
+
+    payload = {
+        "items": [
+            {
+                "type": type,
+                # The storage key where the file is stored.
+                "key": AWS_file_name,
+                # This is the display name of the video in the v7 web UI
+                "filename": AWS_file_name,
+            }
+        ],
+        "storage_name": storage_name
+    }
+
+    response = requests.put(
+        f"https://darwin.v7labs.com/api/teams/{team_slug}/datasets/{dataset_slug}/data",
+        headers=headers,
+        json=payload
+    )
+
+    if response.status_code != 200:
+        print("request failed", response.text)
+    else:
+        print("success")
+
+def main():
+
+    n = len(sys.argv)
+    print("Total arguments passed:", n)
+
+    if (n<3):
+        print("no arguments passed...exiting")
+        exit()
+
+    type = sys.argv[1]
+
+    print("uploading type: ", type)
+
+    if(type != "batch"):
+
+        #python3 AWS_darwin_upload.py video video_path
+        print("data type: ", type)
+
+        for i in range(2, n):
+
+            print("uploading...", sys.argv[i], end=" ")
+
+            upload(type=type, AWS_file_name=sys.argv[i])
+
+    elif(type == "batch"):
+
+        #python3 AWS_darwin_upload.py batch video file_w_video_paths
+
+        if(len(sys.argv) < 4):
+            print("upload type batch but not enough arguments...exiting")
+            exit()
+
+        type = sys.argv[2]
+        print("data type: ", type)
+        print("data file: ", sys.argv[3])
+
+        with open(sys.argv[3], "r", encoding="utf-8") as file:
+
+            for line in file:
+
+                file_name = line.strip()
+                upload(type=type, AWS_file_name=file_name)
+
+
+if __name__ == "__main__":
+    main()
